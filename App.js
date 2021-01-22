ac5385b571f6cf1c07c3f0dc6db2f77d89d4d412
William Moreno <Alejandro>
2021-01-21 23:49:06 -0500

    react native elements installed and Login HALFWAY

--- App.js
+++ App.js
@@ -1,51 +1,48 @@
 import * as React from 'react';
-import {StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 //Navigation
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 //Components
-import CreateUser from "./screens/CreateUser";
+import Login from './screens/Login';
+import CreateUser from './screens/CreateUser';
 
 const Stack = createStackNavigator();
 
-export default function App() {
-  function MyStack() {
-    return (
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#621FF7",
-          },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        }}
-      >
-        <Stack.Screen
-          name="CreateUserScreen"
-          component={CreateUser}
-          options={{ title: "Create a New User" }}
-        />
-      </Stack.Navigator>
-    );
-  }
-  return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
-  );
+export default function App () {
+	function MyStack () {
+		return (
+			<Stack.Navigator
+				screenOptions={{
+					headerStyle      : {
+						backgroundColor : '#e5e500'
+					},
+					headerTintColor  : 'black',
+					headerTitleStyle : {
+						fontWeight : 'bold'
+					}
+				}}
+			>
+				<Stack.Screen name="Login" component={Login} />
+				<Stack.Screen name="Create User" component={CreateUser} options={{ title: 'Create a New User' }} />
+			</Stack.Navigator>
+		);
+	}
+	return (
+		<NavigationContainer>
+			<MyStack />
+		</NavigationContainer>
+	);
 }
 
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container : {
+		flex            : 1,
+		backgroundColor : '#fff',
+		alignItems      : 'center',
+		justifyContent  : 'center'
+	}
 });
