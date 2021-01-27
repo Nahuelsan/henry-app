639750a13ae6a95ddb1a470bc5746899c5fe2910
William Moreno <Alejandro>
2021-01-27 01:30:49 -0500

    profile

--- screens/profile.js
+++ screens/profile.js
@@ -1,23 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView, StyleSheet, ActivityIndicator, TextInput, Button, Alert } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Picker, ActivityIndicator, TextInput, Button, Alert } from 'react-native';
 import { Avatar, Icon } from 'react-native-elements';
 import firebase from '../database/database';
 
 const Profile = (props) => {
 	const { info } = props.route.params;
 	const { navigation } = props;
+
 	const [
 		user,
 		setUser
-	] = useState({});
+	] = useState(info);
+
 	const [
 		edit,
 		setEdit
 	] = useState(false);
+
+	const [
+		selectedRol,
+		setSelectedRol
+	] = useState('');
+
 	const dbRef = firebase.db.collection('users').doc(info.id);
+	/* 
 	useEffect(() => {
 		setUser(info);
-	}, []);
+	}, []); */
 
 	const profile = [
 		{
@@ -46,6 +55,44 @@ const Profile = (props) => {
 			: await dbRef.set(user);
 	};
 
+	const eliminar = async () => {
+		if (confirm('Esta seguro de querer eliminar este Usuario')) {
+			await dbRef.delete();
+			alert('Usuario Eiminado');
+			navigation.navigate('Henry Admin');
+		}
+		else {
+		}
+	};
+
+	const setPermisos = async (rol) => {
+		if (confirm('Esta seguro del cambio de Perfil de Usuario')) {
+			await dbRef.set({ ...user, rol: rol });
+			setUser({ ...user, rol: rol });
+		}
+		else {
+		}
+
+		/* Alert.alert(
+			'Cambiar Perfil Usuario',
+			'Esta seguro del cambio de Permisos de Usuario',
+			[
+				{
+					text    : 'Cancel',
+					onPress : () => console.log('Cancel Pressed'),
+					style   : 'cancel'
+				},
+				{
+					text    : 'OK',
+					onPress : async () => {
+						await dbRef.set({ ...user, rol: rol });
+					}
+				}
+			],
+			{ cancelable: false }
+		); */
+	};
+
 	return (
 		<ScrollView style={styles.container_main}>
 			<View style={styles.header}>
@@ -65,8 +112,10 @@ const Profile = (props) => {
 				<View style={styles.name}>
 					<Text style={styles.title}>{`${user.first_name} ${user.last_name}`}</Text>
 					<Text style={styles.subtitle}>{user.email}</Text>
+					<Text style={styles.subtitle}>Perfil: {user.rol}</Text>
 				</View>
 			</View>
+
 			<View style={styles.datos}>
 				<Text style={{ fontWeight: 'bold', fontSize: 15 }}>Datos</Text>
 				{profile.map((field) => (
@@ -91,6 +140,32 @@ const Profile = (props) => {
 						setEdit(!edit), edit && updateUser();
 					}}
 				/>
+			</View>
+
+			<View style={styles.datos}>
+				<Text style={{ fontWeight: 'bold', fontSize: 15 }}>Actualizar Permisos de Usuario</Text>
+				<Text style={{ fontSize: 15 }}>Selecciona el rol del usuario</Text>
+				<Picker
+					/* selectedRol={selectedRol}
+					 */ style={{ height: 50, width: 150 }}
+					onValueChange={(itemValue, itemIndex) => setSelectedRol(itemValue)}
+				>
+					<Picker.Item label="Selección" value="" />
+					{user.rol !== 'student' ? <Picker.Item label="Estudiante" value="student" /> : null}
+					{user.rol !== 'pm' ? <Picker.Item label="PM" value="pm" /> : null}
+					{user.rol !== 'instructor' ? <Picker.Item label="Instructor" value="instructor" /> : null}
+					{user.rol !== 'admin' ? <Picker.Item label="Administrador" value="admin" /> : null}
+				</Picker>
+
+				<Button
+					title={selectedRol ? 'Actualizar' : 'Seleccione una opción'}
+					disabled={selectedRol ? false : true}
+					onPress={() => setPermisos(selectedRol)}
+				/>
+			</View>
+
+			<View style={styles.datos}>
+				<Button style={{ backgroundColor: 'red' }} title="Eliminar Usuario" onPress={() => eliminar()} />
 			</View>
 		</ScrollView>
 	);
