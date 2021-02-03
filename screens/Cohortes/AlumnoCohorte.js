b25d6bce4a91a500fe9709926eda06e2e270413e
Nahuelsan <nahuel.-sanchez@live.com.ar>
2021-02-03 11:32:41 -0300

    Filtro Alumno

--- screens/Cohortes/AlumnoCohorte.js
+++ screens/Cohortes/AlumnoCohorte.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, Component} from 'react';
 import {Text, ScrollView, Image} from 'react-native';
 import { Icon, ListItem} from 'react-native-elements';
 import {
@@ -26,77 +26,133 @@ import {
   } from './StyledCohorteList';
 import firebase from '../../database/database';
 
-const AlumnosCohorte = ({navigation, route}) => {
+class AlumnosCohorte extends Component {
 
-    const [alumnos, setAlumnos] = useState([])
-
-    useEffect(() => {
-        firebase.db.collection('users').onSnapshot((query) =>{
-            var data = []
-            query.docs.forEach((docs) => {
-                const {cohorte, dni, email, first_name, grupo, last_name, nacionalidad, phone, photo} = docs.data()
-                if(route.params.nombre === cohorte){
-                    data.push({
-                        cohorte, 
-                        dni, 
-                        email, 
-                        first_name, 
-                        grupo, 
-                        last_name, 
-                        nacionalidad, 
-                        phone,
-                        photo
-                    });
-                }
+    state = {
+        alumnos: [],
+        alumnosFiltrados : '',
+        grupos: ['1', '2', '3'],
+        filterActive: false
+    }
+    componentDidMount(){
+            firebase.db.collection('users').onSnapshot((query) =>{
+                var data = []
+                query.docs.forEach((docs) => {
+                    const {cohorte, dni, email, first_name, grupo, last_name, nacionalidad, phone, photo} = docs.data()
+                    if(this.props.route.params.nombre === cohorte){
+                        data.push({
+                            cohorte, 
+                            grupo,
+                            dni, 
+                            email, 
+                            first_name,  
+                            last_name, 
+                            nacionalidad, 
+                            phone,
+                            photo
+                        });
+                    }
+                });
+                this.setState({
+                    alumnos : data,
+                    preAlumnos : data
+                });
+    
             });
-            setAlumnos(data);
-        });
-    }, []);
-    console.log(alumnos)
+    }
+    filtrado(group){
+        const alumnos = [...this.state.alumnos];
+        if(!isNaN(group)){
+            this.setState({
+                 filterActive : true, 
+                alumnosFiltrados : alumnos.filter(alumno => alumno.grupo === group)
+            })
+        }else{
+            this.setState({
+                 filterActive : false, 
+            })
+        }
+        console.log(this.state.alumnos)
+        console.log(this.state.alumnosFiltrados)
+        console.log(this.state.filterActive)
+    }
+    render(){
     return(
         <Contenedor>
             <Encabezado>
                 <ConTitle
-                    onPress={() => navigation.goBack()}
+                    onPress={() => this.props.navigation.goBack()}
                 >          
                 <Icon
                     solid={true}
                     name="chevron-left"
                     type="font-awesome-5"
                 />
-                <TextTitle>Cohorte {route.params.nombre}</TextTitle>
+                <TextTitle>Cohorte {this.props.route.params.nombre}</TextTitle>
                 </ConTitle>
             </Encabezado>
             <Options>
                 <BackImg>
                     <ImgSise/>
                 </BackImg>
-                <ContText>
-                    <TituloCard>Alumnos</TituloCard>
-                    <Text>Listado de los alumnos</Text>
+                    <ContText>
+                        <TituloCard>Alumnos</TituloCard>
+                        <Text>Listado de los alumnos</Text>
                 </ContText>
             </Options>
+
             <ContGeneral>
         <ContListGen>
-          {alumnos.map((l, i) => (
+        <ConTitle
+            onPress={() => this.filtrado("Todos")}
+        > 
+            <TextTitle>Todos</TextTitle>
+        </ConTitle>
+        {
+            this.state.grupos.map((l,i) => (
+                <ConTitle
+                    key={i}
+                    onPress={() => this.filtrado(l)}
+                > 
+                    <TextTitle>Grupo {l}</TextTitle>
+                </ConTitle>
+            ))
+        }
+
+        {
+        this.state.filterActive === false && 
+        this.state.alumnos.map((l, i) => (
             <ListItem key={i} style={{ width: '100%', }}>
               <BodyUnitItem >
-
                   <ImgListUn source={l.photo} />
-
                 <ContText>
                   <TextPrin>{`${l.last_name} ${l.first_name}`}</TextPrin>
                   <Text>{`${l.email}`}</Text>
                 </ContText>
               </BodyUnitItem>
             </ListItem>
-          ))}
+          ))
+        }
+        {
+            this.state.filterActive === true &&
+            this.state.alumnosFiltrados.map((l, i) => (
+                <ListItem key={i} style={{ width: '100%', }}>
+                  <BodyUnitItem >
+                      <ImgListUn source={l.photo} />
+                    <ContText>
+                      <TextPrin>{`${l.last_name} ${l.first_name}`}</TextPrin>
+                      <Text>{`${l.email}`}</Text>
+                    </ContText>
+                  </BodyUnitItem>
+                </ListItem>
+              ))
+        }
         </ContListGen>
             {/* Menu inferior General */}
             
         </ContGeneral>
         </Contenedor>
-    )
+    )}
 }
 
 export default AlumnosCohorte;
