daed87d0fd55ca11514b4c4e92cb90a354d84a45
gonzaloaguilarm <gonzaloaguilarm@gmail.com>
2021-02-04 18:23:32 -0300

    componentes de Inicio y Registro

--- adminweb/src/App.js
+++ adminweb/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+//Components
+import SignIn from './components/SignIn/SignIn.jsx'
+import Register from './components/Register/Register.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={SignIn} />
+        <Route exact path='/register' component={Register} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
