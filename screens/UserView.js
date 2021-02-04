4ee08c5f8656150e10400927527cc7dbece7fae9
Nahuelsan <nahuel.-sanchez@live.com.ar>
2021-02-04 11:22:30 -0300

    Redux

--- screens/UserView.js
+++ screens/UserView.js
@@ -4,12 +4,12 @@ import UserBody from './UserBody';
 import UserHeader from './UserHeader';
 import UserNavBar from './UserNavBar';
 import Footer from './Footer';
+import {useSelector} from 'react-redux';
 
 const UserView = (props) => {
-	const { info } = props.route.params;
+	const info = useSelector(state => state)
 	return (
 		<View>
-			{console.log(info)}
 			<UserHeader info={info} navigation={props.navigation}/>
 			<UserNavBar info={info} />
 			<UserBody info={info} />
